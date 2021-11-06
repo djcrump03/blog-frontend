5cf237b7b729e61cd84acbfeb76a4477fd404ae8
djcrump03 <64335020+djcrump03@users.noreply.github.com>
2021-11-06 17:33:35 -0500

    initial setup

---
+++ blog-frontend/src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Blog App</h1>
+        <h2>By Dionna Crump</h2>
+      </div>
+    );
+  }
+}
